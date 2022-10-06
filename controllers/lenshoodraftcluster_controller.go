db4e2868989061f8db5c7748bf8b5d442e7af410
zhangxuhai <lenshood.zxh@gmail.com>
2022-10-06 18:50:45 +0800

    feat: create service and pod

--- controllers/lenshoodraftcluster_controller.go
+++ controllers/lenshoodraftcluster_controller.go
@@ -18,13 +18,18 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/klog/v2"
+	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"strings"
 
 	. "go-raft-operator/api/v1"
 )
@@ -65,10 +70,10 @@ func (r *LenshoodRaftClusterReconciler) Reconcile(ctx context.Context, req ctrl.
 	}
 
 	if len(cluster.Status.Ids) == 0 {
-		return res, r.createSeedToCluster(ctx, cluster.DeepCopy())
+		return res, r.createCluster(ctx, cluster.DeepCopy())
 	}
 
-	return ctrl.Result{}, r.joinToCluster(ctx, cluster.DeepCopy())
+	return ctrl.Result{}, r.updateCluster(ctx, cluster.DeepCopy())
 }
 
 func (r *LenshoodRaftClusterReconciler) reachedDesiredState(ctx context.Context, cluster *LenshoodRaftCluster) bool {
@@ -89,11 +94,71 @@ func (r *LenshoodRaftClusterReconciler) reachedDesiredState(ctx context.Context,
 	return true
 }
 
-func (r *LenshoodRaftClusterReconciler) createSeedToCluster(ctx context.Context, cluster *LenshoodRaftCluster) error {
+func getPodName(ordinal int) string {
+	return fmt.Sprintf("lenshood-raft-%d", ordinal)
+}
+
+func buildRaftHeadlessSvc(cluster *LenshoodRaftCluster) *v1.Service {
+	svcYaml, err := os.ReadFile("config/svc/raft-svc.yaml")
+	if err != nil {
+		klog.Fatalf("read pod yaml file failed: %v", err)
+	}
+
+	svc := &v1.Service{}
+	if err := yaml.Unmarshal(svcYaml, svc); err != nil {
+		klog.Fatalf("unmarshal pod yaml file failed: %v", err)
+	}
+
+	svc.Namespace = cluster.Namespace
+	svc.Spec.Selector = map[string]string{"managed-by": cluster.Name}
+	return svc
+}
+
+func buildRaftPod(cluster *LenshoodRaftCluster, members []string, ordinal int) *v1.Pod {
+	podYaml, err := os.ReadFile("config/pod/raft-pod.yaml")
+	if err != nil {
+		klog.Fatalf("read pod yaml file failed: %v", err)
+	}
+
+	pod := &v1.Pod{}
+	if err := yaml.Unmarshal(podYaml, pod); err != nil {
+		klog.Fatalf("unmarshal pod yaml file failed: %v", err)
+	}
+
+	pod.ObjectMeta.Labels = map[string]string{"managed-by": cluster.Name}
+	pod.Namespace = cluster.Namespace
+	pod.Name = getPodName(ordinal)
+	pod.Spec.Hostname = getPodName(ordinal)
+	pod.Spec.Containers[0].Image = cluster.Spec.Image
+	pod.Spec.Containers[0].Env = []v1.EnvVar{{Name: "ME_ADDR", Value: members[ordinal]}, {Name: "MEMBERS_ADDR", Value: strings.Join(members, ",")}}
+
+	return pod
+}
+
+func (r *LenshoodRaftClusterReconciler) createCluster(ctx context.Context, cluster *LenshoodRaftCluster) error {
+	svc := buildRaftHeadlessSvc(cluster)
+	if err := r.Create(ctx, svc); err != nil {
+		return err
+	}
+
+	podNums := cluster.Spec.Replica
+	members := make([]string, 0, podNums)
+	for i := 0; i < podNums; i++ {
+		members = append(members, getPodName(i)+"."+svc.Name+":34220")
+	}
+
+	for i := 0; i < cluster.Spec.Replica; i++ {
+		pod := buildRaftPod(cluster, members, i)
+		if err := r.Create(ctx, pod); err != nil {
+			return err
+		}
+		cluster.Status.Ids = append(cluster.Status.Ids, pod.Name)
+	}
+
 	return nil
 }
 
-func (r *LenshoodRaftClusterReconciler) joinToCluster(ctx context.Context, cluster *LenshoodRaftCluster) error {
+func (r *LenshoodRaftClusterReconciler) updateCluster(ctx context.Context, cluster *LenshoodRaftCluster) error {
 	return nil
 }
 
